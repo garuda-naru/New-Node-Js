fcc14b837e724a7021ea56e2711a69eec21d6367
Narender <chinna.govindula@gmail.com>
2022-12-21 21:55:05 +0530

    HttpRequest

---
+++ HttpReq.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+http.get('http://api.open-notify.org/astros.json',res => {
+
+    let data = '';
+    res.on('data',chunk => {
+        data+= chunk
+    });
+
+    res.on('end', ()=> {
+        let jsondata = JSON.parse(data)
+        console.log(jsondata)
+    })
+})
