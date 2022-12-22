934e1fd2831dfdb0f99da974d222e40c5ef1e1c9
Narender <chinna.govindula@gmail.com>
2022-12-22 11:59:10 +0530

    axios

--- HttpPostReq.js
+++ HttpPostReq.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const http = require('http');
+const axios = require('axios');
 
 const data = JSON.stringify({
     name: "Narender",
@@ -14,19 +15,20 @@ const options = {
     }
 }
 
-const req = http.request(options,(res)=> {
-    let body = '';
-    console.log('Status Code:',res.statusCode)
+// const req = http.request(options,(res)=> {
+//     let body = '';
+//     console.log('Status Code:',res.statusCode)
+//     res.on('data',(chunk)=> {
+//         body+= chunk;
+//     })
+//     res.on('end',()=>{
+//         console.log('Body:',JSON.parse(data));
+//     })
+// })
 
-
-    res.on('data',(chunk)=> {
-        body+= chunk;
-    })
-
-    res.on('end',()=>{
-        console.log('Body:',JSON.parse(data));
-    })
+axios.post('https://reqres.in/api/users',data).then(res=> {
+    console.log(`Status Code :$(res.status)`);
+    console.log(`Body : ${JSON.stringify(res.data)}`);
+}).catch(err => {
+    console.log(err)
 })
-
-req.write(data)
-req.end()
