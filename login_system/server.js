ff454ce5bb542b718e4a46ef2ef058bd531402d9
Narender <chinna.govindula@gmail.com>
2022-12-28 10:12:59 +0530

    Login System

---
+++ login_system/server.js
@@ -0,0 +1,12 @@
+ const express = require('express');
+ const app = express();
+
+ const port = process.env.PORT || 3000;
+app.set('view engine','ejs');
+
+ //home route
+ app.get('/',(req,res)=> {
+    res.render('base',{title:"Login System"});
+ })
+
+ app.listen(port,()=> {console.log("Server running at http://localhost:3000")})
