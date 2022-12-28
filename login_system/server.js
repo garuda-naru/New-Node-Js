7069b7bf650b7319bae944345b86d1e5c13615aa
Narendra <59531235+garuda-naru@users.noreply.github.com>
2022-12-28 07:17:50 +0000

    new

--- login_system/server.js
+++ login_system/server.js
@@ -1,8 +1,12 @@
  const express = require('express');
+ const path = require('path');
  const app = express();
 
  const port = process.env.PORT || 3000;
 app.set('view engine','ejs');
+
+//load static data
+app.use('/static',express.static(path.join(__dirname,'public')))
 
  //home route
  app.get('/',(req,res)=> {
